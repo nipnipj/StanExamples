46eb09d4667d34d378eaac6c2324af67226ffadb
N!P <36553373+nipnipj@users.noreply.github.com>
2024-06-21 22:50:58 -0500

    Update GLM_poisson.R

--- GLM_poisson/GLM_poisson.R
+++ GLM_poisson/GLM_poisson.R
@@ -23,4 +23,3 @@ mod <- cmdstan_model("~/dtSTAN/GLM_poisson/model.stan")
 fit <- mod$sample(data = dt, chains = 2, parallel_chains = 2, adapt_delta = 0.9, 
                   iter_warmup = 1000,  iter_sampling = 500, max_treedepth = 10)
 fit$print(c("Intercept", "beta"))
-fit$print(c("yrep"))
